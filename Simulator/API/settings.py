d466b4789bed57914b956c4a7d2d0861fb9b092d
James <jimmy.levell@bluewin.ch>
2022-11-07 17:24:12 +0100

    Fixed simulation issue

--- Simulator/API/settings.py
+++ Simulator/API/settings.py
@@ -1,18 +1,18 @@
 def init():
     global carla_port
-    carla_port = 3001
+    carla_port = 3000
 
     global carla_download_path
     carla_download_path = "https://download.app.levell.ch/crowdmanager/CARLA_0.9.13.zip"
 
     global number_of_cameras
-    number_of_cameras = 3
+    number_of_cameras = 2
 
     global number_of_vehicles
-    number_of_vehicles = 10
+    number_of_vehicles = 100
 
     global number_of_walkers
-    number_of_walkers = 10
+    number_of_walkers = 100
 
     global percentage_pedestrians_running
     percentage_pedestrians_running = 0.0  # how many pedestrians will run
